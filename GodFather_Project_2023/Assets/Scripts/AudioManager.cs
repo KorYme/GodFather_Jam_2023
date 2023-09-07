fcdba3c8f12765b86feeca4e54a5165a82b38175
Paul Putegnat <paul.putegnat@edu.devinci.fr>
2023-09-07 13:51:28 +0200

    Added crowd jumping + AudioManager

---
+++ GodFather_Project_2023/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,96 @@
+using UnityEngine.Audio;
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] Slider _volumeSlider;
+
+    public Sound[] Sounds;
+
+    public static AudioManager Instance;
+
+    private AudioSource _audioSource;
+
+    private AudioClip[] _exempleListAudioClip;
+
+    private int _randomSoundNum;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound sound in Sounds)
+        {
+            sound.Source = gameObject.AddComponent<AudioSource>();
+            sound.Source.clip = sound.Clip;
+
+            sound.Source.volume = sound.Volume;
+            sound.Source.pitch = sound.Pitch;
+            sound.Source.loop = sound.Loop;
+        }
+    }
+
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        _exempleListAudioClip = Resources.LoadAll<AudioClip>("SFX_Exemple_List_AudioClip");
+    }
+
+    public void PlaySingleSound(string name)
+    {
+        Sound s = Array.Find(Sounds, sound => sound.Name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Le son: " + name + " n'existe pas!");
+            return;
+        }
+        s.Source.Play();
+
+        /*
+        Pour jouer un son unique dans d'autres scripts:
+        AudioManager.Instance.PlaySingleSound("nom du son");
+        */
+    }
+
+    public void StopSingleSound(string name)
+    {
+        Sound s = Array.Find(Sounds, sound => sound.Name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+        s.Source.Stop();
+
+        /*
+        Pour stoper un son unique dans d'autres scripts:
+        AudioManager.Instance.StopSingleSound("nom du son");
+        */
+    }
+
+    public void PlayExempleListAudioClip()
+    {
+        _randomSoundNum = Random.Range(0, 3);
+        _audioSource.PlayOneShot(_exempleListAudioClip[_randomSoundNum]);
+    }
+
+    /*
+    Pour jouer un son aléatoire parmi une liste dans d'autres scripts:
+    AudioManager.Instance.PlayExempleListAudioClip();
+    */
+}
--- GodFather_Project_2023/Assets/Scripts/BattleManager.cs
+++ GodFather_Project_2023/Assets/Scripts/BattleManager.cs
@@ -21,7 +21,7 @@ public class BattleManager : MonoBehaviour
     {
         bubbleRight = GameObject.Find("/Canvas/BubbleRight");
         bubbleLeft = GameObject.Find("/Canvas/BubbleLeft");
-        crowd = GameObject.Find("/Canvas/Crowd");
+        crowd = GameObject.Find("/Crowd");
     }
 
     void Start()
@@ -42,12 +42,14 @@ public class BattleManager : MonoBehaviour
 
     public IEnumerator P1Punchline()
     {
+        Debug.Log("P1");
         if(scoreP1 >= punchlineScoreNeeded)
         {
             scoreP1 -= punchlineScoreNeeded;
             bubbleLeft.SetActive(true);
             yield return new WaitForSeconds(durationOfThePunchline);
             bubbleLeft.SetActive(false);
+            StartCoroutine(CrowdJump());
             yield return new WaitForSeconds(timeBetweenPunchline);
         }
         else
@@ -61,12 +63,14 @@ public class BattleManager : MonoBehaviour
 
     public IEnumerator P2Punchline()
     {
+        Debug.Log("P2");
         if (scoreP2 >= punchlineScoreNeeded)
         {
             scoreP2 -= punchlineScoreNeeded;
             bubbleRight.SetActive(true);
             yield return new WaitForSeconds(durationOfThePunchline);
             bubbleRight.SetActive(false);
+            StartCoroutine(CrowdJump());
             yield return new WaitForSeconds(timeBetweenPunchline);
         }
         else
@@ -76,5 +80,31 @@ public class BattleManager : MonoBehaviour
             yield return null;
         }
         StartCoroutine(P1Punchline());
+    }
+
+    private IEnumerator CrowdJump()
+    {
+        Vector2 originalPos = new Vector2(crowd.transform.position.x, crowd.transform.position.y);
+        Vector2 targetedPos = new Vector2(crowd.transform.position.x, crowd.transform.position.y + 1);
+
+        float lerp = 0;
+
+        while (lerp < 1)
+        {
+            lerp += Time.deltaTime / .2f;
+            crowd.transform.position = Vector2.Lerp(originalPos, targetedPos, lerp);
+
+            yield return null;
+        }
+
+        lerp = 0;
+
+        while (lerp < 1)
+        {
+            lerp += Time.deltaTime / .2f;
+            crowd.transform.position = Vector2.Lerp(targetedPos, originalPos, lerp);
+
+            yield return null;
+        }
     }
 }
