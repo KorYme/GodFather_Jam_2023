05db3dbbd901b78ead85faab1d5e0b0a5d7d34e3
KorYme <maxime.corby.pro@gmail.com>
2023-09-08 09:26:20 +0200

    [UPD] : BattleManager

--- GodFather_Project_2023/Assets/Scripts/BattleManager.cs
+++ GodFather_Project_2023/Assets/Scripts/BattleManager.cs
@@ -2,24 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using static System.Net.Mime.MediaTypeNames;
 
 public class BattleManager : MonoBehaviour
 {
-    public int scoreP1;
-    public int scoreP2;
-    public int punchlineScoreNeeded;
     public float timeBetweenPunchline;
     public float durationOfThePunchline;
 
     private ReactionScript reactionScript;
     [SerializeField] MappingDictionnarySO _mapping;
-    [SerializeField] List<UnityEngine.UI.Image> _allImages;
+    [SerializeField] List<Image> _allImages;
     [SerializeField] List<Sprite> _bubbleSprites;
-    [SerializeField] UnityEngine.UI.Image bubble;
-
-    private bool P1NoMorePunchline;
-    private bool P2NoMorePunchline;
+    [SerializeField] Image bubble;
+    public int Winner { get; private set; }
 
     private void Awake()
     {
@@ -28,51 +22,9 @@ public class BattleManager : MonoBehaviour
 
     void Start()
     {
+        Winner = -1;
         bubble.gameObject.SetActive(false);
         StartCoroutine(PunchlineCoroutine());
-    }
-
-    void Update()
-    {
-        if(P1NoMorePunchline && P2NoMorePunchline == true)
-        {
-            StopAllCoroutines();
-            Debug.Log("Finished!");
-        }
-    }
-
-    public IEnumerator P1Punchline()
-    {
-        int pictoBatch = ScoreManager.Instance.PictoPerBatch[ScoreManager.Instance.Round];
-        if (ScoreManager.Instance.CurrentPlayerQueue.Count >= pictoBatch)
-        {
-            List<string> list = new List<string>();
-            for (int i = 0; i < pictoBatch; i++)
-            {
-                list.Add(ScoreManager.Instance.CurrentPlayerQueue.Dequeue());
-            }
-            for (int i = 0; i < list.Count; i++)
-            {
-                _allImages[i].enabled = true;
-                _allImages[i].sprite = _mapping.Values[_mapping.Keys.FindIndex(x => x.ToLower() == list[i].ToLower())];
-            }
-            for (int i = list.Count; i < 12; i++)
-            {
-                _allImages[i].enabled = false;
-            }
-            //bubble.SetActive(true);
-            yield return new WaitForSeconds(durationOfThePunchline);
-            //bubble.SetActive(false);
-            reactionScript.AllCrowdEffects();
-            yield return new WaitForSeconds(timeBetweenPunchline);
-        }
-        else
-        {
-            //Jouer une animation d'hésitation / déçu
-            P1NoMorePunchline = true;
-            yield return null;
-        }
-        StartCoroutine(P2Punchline());
     }
 
     public IEnumerator PunchlineCoroutine()
@@ -131,35 +83,7 @@ public class BattleManager : MonoBehaviour
             yield return new WaitForSeconds(timeBetweenPunchline);
         }
         //Jouer une animation d'hésitation / déçu
-        if (ScoreManager.Instance.CurrentPlayer == 0)
-        {
-            //Victoir joueur 1
-        }
-        else
-        {
-            //Victoir joueur 2
-        }
-        P1NoMorePunchline = true;
+        Winner = ScoreManager.Instance.CurrentPlayer;
         yield return null;
-    }
-
-    public IEnumerator P2Punchline()
-    {
-        if (scoreP2 >= punchlineScoreNeeded)
-        {
-            scoreP2 -= punchlineScoreNeeded;
-            //bubble.SetActive(true);
-            yield return new WaitForSeconds(durationOfThePunchline);
-            //bubble.SetActive(false);
-            reactionScript.AllCrowdEffects();
-            yield return new WaitForSeconds(timeBetweenPunchline);
-        }
-        else
-        {
-            //Jouer une animation d'hésitation / déçu
-            P2NoMorePunchline = true;
-            yield return null;
-        }
-        StartCoroutine(P1Punchline());
     }
 }
